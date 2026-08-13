490f22ad4cba929e3ef74f83b1fd7f22a1c28b6a
Sebss <samuelrd683@gmail.com>
2026-08-12 23:09:28 -0500

    Fix import path for configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import '/config';
 import { requireAuth } from './auth.js';
 import { secretsRouter } from './secrets.js';
 
